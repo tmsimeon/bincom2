8b2b6ef5566d1147937241e4cc6264e3f1d121b2
tmsimeon <simeonmnaan@gmail.com>
2025-09-09 11:00:02 +0100

    push files online

---
+++ test.py
@@ -0,0 +1,8 @@
+authors: {('Jim Henson', '/author/Jim-Henson'), ('Albert Einstein', '/author/Albert-Einstein'), 
+          ('Bob Marley', '/author/Bob-Marley'), ('Thomas A. Edison', '/author/Thomas-A-Edison'), 
+          ('André Gide', '/author/Andre-Gide'), ('Marilyn Monroe', '/author/Marilyn-Monroe'), 
+          ('Steve Martin', '/author/Steve-Martin'), ('Ralph Waldo Emerson', '/author/Ralph-Waldo-Emerson'), 
+          ('Dr. Seuss', '/author/Dr-Seuss'), ('Mother Teresa', '/author/Mother-Teresa'), ('Eleanor Roosevelt', '/author/Eleanor-Roosevelt'), 
+          ('Friedrich Nietzsche', '/author/Friedrich-Nietzsche'), ('Allen Saunders', '/author/Allen-Saunders'), ('Pablo Neruda', '/author/Pablo-Neruda'), 
+          ('Elie Wiesel', '/author/Elie-Wiesel'), ('Mark Twain', '/author/Mark-Twain'), ('Jane Austen', '/author/Jane-Austen'),
+            ('Garrison Keillor', '/author/Garrison-Keillor'), ('Douglas Adams', '/author/Douglas-Adams'), ('J.K. Rowling', '/author/J-K-Rowling')}
